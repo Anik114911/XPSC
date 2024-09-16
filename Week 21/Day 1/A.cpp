694b59d93280e3492fb8aad092411fbd45dcbc2e
Anik114911 <147538481+Anik114911@users.noreply.github.com>
2024-09-16 23:21:22 +0600

    Add files via upload
    week 21 day 1 codes

---
+++ Week 21/Day 1/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long 
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define e4 int32_t main()
+#define Infinite void
+#define PI 2.0 * acos(0.0)
+using namespace std;
+#ifdef LOKAL
+#include "Debug_Template.h"
+#else
+#define HERE
+#define debug(args...) 
+#endif 
+using namespace __gnu_pbds;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+Infinite Void() {
+    int n;
+    cin >> n;
+    string s;
+    int i = 1;
+    while(i <= n){
+        if(i == 1) s.push_back('a');
+        else if(i == 2) s.push_back('e');
+        else if(i == 3) s.push_back('i');
+        else if(i == 4) s.push_back('o');
+        else if(i == 5) s.push_back('u');
+        if(i == 5){
+            n -= 5;
+            i = 0;
+        }
+        i++;
+    }
+    sort(s.begin(),s.end());
+    cout << s << endl;
+}
+e4{
+    fst int t = 1;
+    cin >> t;
+    for (int i=1;i<=t;++i){
+        // cout << "Case " << i << ": ";
+        Void();
+    }
+    return 0;
+}
