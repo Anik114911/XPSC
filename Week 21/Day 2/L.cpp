e3b0f4f26a0b5c9314e3463e4ab3df67030ff49e
Anik114911 <147538481+Anik114911@users.noreply.github.com>
2024-09-17 22:36:44 +0600

    Add files via upload
    week 21 day 2 codes

---
+++ Week 21/Day 2/L.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fst                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long 
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define e4 int32_t main()
+#define Infinite void
+#define PI 2.0 * acos(0.0)
+using namespace std;
+#ifdef LOKAL
+#include "Debug_Template.h"
+#else
+#define HERE
+#define debug(args...) 
+#endif 
+using namespace __gnu_pbds;
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+Infinite Void() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    bool ok = true;
+    int mn = INT_MAX, idx = -1;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        if(a[i] < mn){
+            mn = a[i];
+            idx = i;
+        }
+    }
+    for(int i=1;i<n;i++){
+        if(a[i] < a[i-1]){
+            ok = false;
+            break;
+        }
+    }
+    if(ok){
+        cout << "0" << endl;
+        return;
+    }
+    // cout << idx << " -> ";
+    for(int i=idx+1;i<n;i++){
+        if(a[i] < a[i-1]){
+            cout << "-1" << endl;
+            return;
+        }
+    }
+    cout << idx << endl;
+}
+e4{
+    fst int t = 1;
+    cin >> t;
+    for (int i=1;i<=t;++i){
+        // cout << "Case " << i << ": ";
+        Void();
+    }
+    return 0;
+}
